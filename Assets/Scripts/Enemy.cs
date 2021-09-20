0913d2be7d0e3d4efd52811d24c8ca5ab9b378dd
faiznailun <m.faiz.nailun@gmail.com>
2021-09-20 17:30:12 +0700

    tower defense

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Enemy : MonoBehaviour
+
+{
+
+    [SerializeField] private int _maxHealth = 1;
+
+    [SerializeField] private float _moveSpeed = 1f;
+
+    [SerializeField] private SpriteRenderer _healthBar;
+
+    [SerializeField] private SpriteRenderer _healthFill;
+
+
+
+    private int _currentHealth;
+
+
+    public Vector3 TargetPosition { get; private set; }
+
+    public int CurrentPathIndex { get; private set; }
+
+    public Enemy(float moveSpeed)
+    {
+        _moveSpeed = moveSpeed;
+    }
+
+    public Enemy(SpriteRenderer healthFill)
+    {
+        _healthFill = healthFill;
+    }
+
+    public SpriteRenderer HealthBar { get => _healthBar; set => _healthBar = value; }
+
+    public override bool Equals(object obj)
+    {
+        var enemy = obj as Enemy;
+        return enemy != null &&
+               base.Equals(obj) &&
+               EqualityComparer<SpriteRenderer>.Default.Equals(HealthBar, enemy.HealthBar);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+
+
+    // Fungsi ini terpanggil sekali setiap kali menghidupkan game object yang memiliki script ini
+
+    private void OnEnable()
+
+    {
+
+        _currentHealth = _maxHealth;
+
+        _healthFill.size = HealthBar.size;
+
+    }
+
+    public void MoveToTarget()
+
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, _moveSpeed * Time.deltaTime);
+
+    }
+
+
+
+    public void SetTargetPosition(Vector3 targetPosition)
+
+    {
+
+        TargetPosition = targetPosition;
+
+        _healthBar.transform.parent = null;
+
+
+
+        // Mengubah rotasi dari enemy
+
+        Vector3 distance = TargetPosition - transform.position;
+
+        if (Mathf.Abs(distance.y) > Mathf.Abs(distance.x))
+
+        {
+
+            // Menghadap atas
+
+            if (distance.y > 0)
+
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
+
+            }
+
+            // Menghadap bawah
+
+            else
+
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -90f));
+
+            }
+
+        }
+
+        else
+
+        {
+
+            // Menghadap kanan (default)
+
+            if (distance.x > 0)
+
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+
+            }
+
+            // Menghadap kiri
+
+            else
+
+            {
+
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
+
+            }
+
+        }
+
+
+        public void ReduceEnemyHealth(int damage)
+
+        {
+
+            _currentHealth -= damage;
+
+            AudioPlayer.Instance.PlaySFX("hit-enemy");
+
+            if (_currentHealth <= 0)
+
+            {
+
+                gameObject.SetActive(false);
+
+                AudioPlayer.Instance.PlaySFX("enemy-die");
+
+            }
+
+        }
+
+
+
+        _healthBar.transform.parent = transform;
+
+    }
+
+
+
+    // Menandai indeks terakhir pada path
+
+    public void SetCurrentPathIndex(int currentIndex)
+
+    {
+
+        CurrentPathIndex = currentIndex;
+
+    }
+
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,283 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+
+
+public class Tower : MonoBehaviour
+
+{
+
+    // Tower Component
+
+    [SerializeField] private SpriteRenderer _towerPlace;
+
+    [SerializeField] private SpriteRenderer _towerHead;
+
+
+
+    // Tower Properties
+
+    [SerializeField] private int _shootPower = 1;
+
+    [SerializeField] private float _shootDistance = 1f;
+
+    [SerializeField] private float _shootDelay = 5f;
+
+    [SerializeField] private float _bulletSpeed = 1f;
+
+    [SerializeField] private float _bulletSplashRadius = 0f;
+
+
+    [SerializeField] private Bullet _bulletPrefab;
+
+
+
+    private float _runningShootDelay;
+
+    private Enemy _targetEnemy;
+
+    private Quaternion _targetRotation;
+
+
+
+    // Mengecek musuh terdekat
+
+    public void CheckNearestEnemy(List<Enemy> enemies)
+
+    {
+
+        if (_targetEnemy != null)
+
+        {
+
+            if (!_targetEnemy.gameObject.activeSelf || Vector3.Distance(transform.position, _targetEnemy.transform.position) > _shootDistance)
+
+            {
+
+                _targetEnemy = null;
+
+            }
+
+            else
+
+            {
+
+                return;
+
+            }
+
+        }
+
+
+
+        float nearestDistance = Mathf.Infinity;
+
+        Enemy nearestEnemy = null;
+
+
+
+        foreach (Enemy enemy in enemies)
+
+        {
+
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distance > _shootDistance)
+
+            {
+
+                continue;
+
+            }
+
+
+
+            if (distance < nearestDistance)
+
+            {
+
+                nearestDistance = distance;
+
+                nearestEnemy = enemy;
+
+            }
+
+        }
+
+
+
+        _targetEnemy = nearestEnemy;
+
+    }
+
+
+
+    // Menembak musuh yang telah disimpan sebagai target
+
+    public void ShootTarget()
+
+    {
+
+        if (_targetEnemy == null)
+
+        {
+
+            return;
+
+        }
+
+
+
+        _runningShootDelay -= Time.unscaledDeltaTime;
+
+        if (_runningShootDelay <= 0f)
+
+        {
+
+            bool headHasAimed = Mathf.Abs(_towerHead.transform.rotation.eulerAngles.z - _targetRotation.eulerAngles.z) < 10f;
+
+            if (!headHasAimed)
+
+            {
+
+                return;
+
+            }
+
+
+
+            Bullet bullet = LevelManager.Instance.GetBulletFromPool(_bulletPrefab);
+
+            bullet.transform.position = transform.position;
+
+            bullet.SetProperties(_shootPower, _bulletSpeed, _bulletSplashRadius);
+
+            bullet.SetTargetEnemy(_targetEnemy);
+
+            bullet.gameObject.SetActive(true);
+
+
+
+            _runningShootDelay = _shootDelay;
+
+        }
+
+    }
+
+
+
+    // Membuat tower selalu melihat ke arah musuh
+
+    public void SeekTarget()
+
+    {
+
+        if (_targetEnemy == null)
+
+        {
+
+            return;
+
+        }
+
+
+
+        Vector3 direction = _targetEnemy.transform.position - transform.position;
+
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        _targetRotation = Quaternion.Euler(new Vector3(0f, 0f, targetAngle - 90f));
+
+
+
+        _towerHead.transform.rotation = Quaternion.RotateTowards(_towerHead.transform.rotation, _targetRotation, Time.deltaTime * 180f);
+
+    }
+
+    public Tower(SpriteRenderer towerHead)
+    {
+        _towerHead = towerHead;
+    }
+
+    public Tower(float shootDistance)
+    {
+        _shootDistance = shootDistance;
+    }
+
+    public Tower(int shootPower)
+    {
+        _shootPower = shootPower;
+    }
+
+
+    // Digunakan untuk menyimpan posisi yang akan ditempati selama tower di drag
+
+    public Vector2? PlacePosition { get; private set; }
+    public SpriteRenderer TowerPlace { get => _towerPlace; set => _towerPlace = value; }
+    public float ShootDelay { get => _shootDelay; set => _shootDelay = value; }
+
+    public void SetPlacePosition(Vector2? newPosition)
+
+    {
+
+        PlacePosition = newPosition;
+
+    }
+
+
+
+    public void LockPlacement()
+
+    {
+
+        transform.position = (Vector2)PlacePosition;
+
+    }
+
+
+
+    // Mengubah order in layer pada tower yang sedang di drag
+
+    public void ToggleOrderInLayer(bool toFront)
+
+    {
+
+        int orderInLayer = toFront ? 2 : 0;
+
+        TowerPlace.sortingOrder = orderInLayer;
+
+        _towerHead.sortingOrder = orderInLayer;
+
+    }
+
+
+
+    // Fungsi yang digunakan untuk mengambil sprite pada Tower Head
+
+    public Sprite GetTowerHeadIcon()
+
+    {
+
+        return _towerHead.sprite;
+
+    }
+
+    public override bool Equals(object obj)
+    {
+        var tower = obj as Tower;
+        return tower != null &&
+               base.Equals(obj) &&
+               _bulletSplashRadius == tower._bulletSplashRadius;
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = -206510725;
+        hashCode = hashCode * -1521134295 + base.GetHashCode();
+        hashCode = hashCode * -1521134295 + _bulletSpeed.GetHashCode();
+        return hashCode;
+    }
+}
+
+
